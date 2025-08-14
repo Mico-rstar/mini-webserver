d63d570234b25ef90e261a1f8a84fac9c819cc5a
Rene <2114678406@qq.com>
2025-08-14 19:33:02 +0800

    feat: 增加tracing库日志支持
    feat: 增加连接来源日志

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::net::TcpStream;
 use crate::structs::body::Body;
 use crate::structs::content_type::ContentType;
 use crate::structs::status::Status;
+use tracing::{info, debug, Level};
+use tracing_subscriber::FmtSubscriber;
 
 mod structs;
 mod request;
@@ -13,6 +15,15 @@ mod response;
 
 
 fn main() {
+
+     // 设置日志订阅者
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("设置默认日志订阅者失败");
+
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
@@ -31,6 +42,8 @@ fn main() {
 
 fn handle_connection(stream: &mut TcpStream) -> Result<(), Box<dyn std::error::Error>>{
     let req = request::Request::from_stream(stream)?;
+
+    info!("Connection from {}", req.header().get("Host").unwrap_or(&"unknown host".to_string()));
 
     // 构造响应报文
     let mut res = response::Response::new("mini-webserver/localhost", Status::Ok, ContentType::TEXT);
