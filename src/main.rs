22af316716409295159dd7e9a589455e65f68719
Rene <2114678406@qq.com>
2025-08-16 16:07:23 +0800

    refactor(main): 解耦handle_connection与router创建

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@ use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
-use crate::structs::body::Body;
-use crate::structs::content_type::ContentType;
-use crate::structs::status::Status;
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
@@ -30,10 +27,13 @@ fn main() {
 
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    // 初始化router
+    let router = router_init();
+
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                if let Err(e) = test_router(&mut stream) {
+                if let Err(e) = handle_connection(&mut stream, &router) {
                     eprintln!("{e}");
                 } 
             }
@@ -44,13 +44,16 @@ fn main() {
     }
 }
 
-fn test_router(stream: &mut TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+fn router_init() -> Router {
+    let mut router = Router::new();
+    router.add_route("/api/test", TestAPI);
+    router
+}
+
+fn handle_connection(stream: &mut TcpStream, router: &Router) -> Result<(), Box<dyn std::error::Error>> {
     let req = request::Request::from_stream(stream)?;
 
     info!("Connection from {}: {}", req.header().get("Host").unwrap_or(&"unknown host".to_string()), req.request_line());
-
-    let mut router = Router::new();
-    router.add_route("/api/test", TestAPI);
 
     let res = router.handle_request(&req)?;
     stream.write_all(res.as_bytes().as_slice())?;
